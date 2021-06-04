394b06d56cb0a8bcf347fe140afdee32aa0ec5db
k_andrey_d <k_andrey_d@mail.ru>
2021-06-04 03:32:27 +0300

    update 2.0

--- Detection of artifical texts/algorithm.cpp
+++ Detection of artifical texts/algorithm.cpp
@@ -10,14 +10,14 @@
 #include <boost/python.hpp>
 
 const int T = 10, number_of_gr = 10;
-const int chunk_size = 1000;
+const int chunk_size = 400;
 int chunk_count_human, chunk_count_robot;
 
 int main() {
 	using std::string;
 	using std::vector;
 
-	vector <string> robot_docs, human_docs;
+	vector <string> robot_docs;
 	std::ifstream file;
 	for (int i = 0; i < number_of_gr; ++i) {
 		file.open("C:\\Users\\k_and\\source\\repos\\Detection of artifical texts\\Detection of artifical texts\\RobotTexts\\R" + std::to_string(i + 1) + ".txt", std::ifstream::binary);
@@ -25,22 +25,19 @@ int main() {
 		file.close();
 		prepare(rob_doc);
 		robot_docs.push_back(rob_doc);
-		file.open("C:\\Users\\k_and\\source\\repos\\Detection of artifical texts\\Detection of artifical texts\\HumanTexts\\H" + std::to_string(i + 1) + ".txt", std::ifstream::binary);
-		std::string hum_doc((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-		file.close();
-		prepare(hum_doc);
-		human_docs.push_back(hum_doc);
+		
 	}
-	/*file.open("C:\\Users\\k_and\\source\\repos\\Detection of artifical texts\\Detection of artifical texts\\RobotTexts\\H1.txt", std::ifstream::binary);
+	file.open("C:\\Users\\k_and\\source\\repos\\Detection of artifical texts\\Detection of artifical texts\\HumanTexts\\H1.txt", std::ifstream::binary);
 	std::string target_doc((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
 	file.close();
-	prepare(target_doc);*/
+	prepare(target_doc);
 
-	vector<int> robot_chunks_count, human_chunks_count;
-	vector<vector<string>> robot_chunks(number_of_gr), human_chunks(number_of_gr);
+	vector<int> robot_chunks_count;
+	int human_chunks_count = 0;
+	vector<vector<string>> robot_chunks(number_of_gr);
+	vector<string> human_chunks;
 	for (int i = 0; i < number_of_gr; ++i) {
 		robot_chunks_count.push_back(robot_docs[i].size() / chunk_size);
-		human_chunks_count.push_back(human_docs[i].size() / chunk_size);
 		string temp;
 		string::iterator it;
 		int j;
@@ -52,28 +49,31 @@ int main() {
 			}
 		}
 		temp.clear();
-		for (it = human_docs[i].begin(), j = 1; it != human_docs[i].end(); ++it, ++j) {
-			temp += *it;
-			if (!(j % chunk_size)) {
-				human_chunks[i].push_back(temp);
-				temp.clear();
-			}
+	}
+	string::iterator it;
+	int j = 0;
+	string build_chunk;
+	human_chunks_count = target_doc.size() / chunk_size;
+	for (it = target_doc.begin(), j = 1; it != target_doc.end(); ++it, ++j) {
+		build_chunk += *it;
+		if (!(j % chunk_size)) {
+			human_chunks.push_back(build_chunk);
+			build_chunk.clear();
 		}
 	}
 
-	int total_robot_chunks = 0, total_human_chunks = 0;
+	int total_robot_chunks = 0;
 	for (int i : robot_chunks_count) total_robot_chunks += i;
-	for (int i : human_chunks_count) total_human_chunks += i;
 
 	vector <int> real_lable(total_robot_chunks, 1);
-	for (int i = 0; i < total_human_chunks; ++i) 
+	for (int i = 0; i < human_chunks_count; ++i) 
 		real_lable.push_back(2);
 
 	string doc;
 	for (const std::string& st : robot_docs) 	doc += st;
-	for (const std::string& st : human_docs)	doc += st;
-	//prepare(doc);
-	//n_gram(doc, 5);
+	doc += target_doc;
+	
+	n_gram(doc, 5);
 
 	std::ifstream dictionary_file("dictionary.txt");
 	vector<string> dictionary;
@@ -83,104 +83,92 @@ int main() {
 	}
 	dictionary_file.close();
 
-	std::vector<vector<std::vector<int>>>freq_of_robot_ngramm(number_of_gr), freq_of_human_ngramm(number_of_gr);
+	std::vector<vector<std::vector<int>>>freq_of_robot_ngramm(number_of_gr);
+	vector<vector<int>>freq_of_human_ngramm;
 	for (int i = 0; i < number_of_gr; ++i) {
 		std::vector<std::string>::iterator it;
 
-		for (it = robot_chunks[i].begin(); it != robot_chunks[i].end(); ++it) {
+		for (it = robot_chunks[i].begin(); it != robot_chunks[i].end(); ++it)
 			freq_of_robot_ngramm[i].push_back(freq_in_chunk(*it, dictionary));
-		}
-		for (it = human_chunks[i].begin(); it != human_chunks[i].end(); ++it) {
-			freq_of_human_ngramm[i].push_back(freq_in_chunk(*it, dictionary));
+	}
+	
+	for (vector<string>::iterator chunk_iter = human_chunks.begin(); chunk_iter != human_chunks.end(); ++chunk_iter)
+		freq_of_human_ngramm.push_back(freq_in_chunk(*chunk_iter, dictionary));
+
+	vector <vector<double>> dzv_matrix;
+	vector <double> row;
+
+	/*for the robot docs 1*/
+	for (int i = 0; i < number_of_gr; ++i) {
+		for (int j = T; j < robot_chunks_count[i]; ++j) {
+			for (int k = 0; k < number_of_gr; ++k)
+				if (i != k)
+					for (int n = T; n < robot_chunks_count[k]; ++n)
+						row.push_back(
+							dzv_calc(
+								T, freq_of_robot_ngramm[i][j],
+								freq_of_robot_ngramm[k][n],
+								j, n,
+								freq_of_robot_ngramm[i],
+								freq_of_robot_ngramm[k]
+							));
+
+			/*for all the other docs*/
+			for (int n = T; n < human_chunks_count; ++n)
+				row.push_back(
+					dzv_calc(
+						T, freq_of_robot_ngramm[i][j],
+						freq_of_human_ngramm[n],
+						j, n,
+						freq_of_robot_ngramm[i],
+						freq_of_human_ngramm
+					));
+			dzv_matrix.push_back(row);
+			row.clear();
+
 		}
 	}
 
-	//vector <vector<double>> dzv_matrix;
-	//vector <double> row;
+		/*for the human docs*/
+	for (int j = T; j < human_chunks_count; ++j) {
+		/*for all the other docs*/
+		for (int k = 0; k < number_of_gr; ++k)
+			for (int n = T; n < robot_chunks_count[k]; ++n)
+				row.push_back(
+					dzv_calc(
+						T, freq_of_human_ngramm[j],
+						freq_of_robot_ngramm[k][n],
+						j, n,
+						freq_of_human_ngramm,
+						freq_of_robot_ngramm[k]
+					));
+		dzv_matrix.push_back(row);
+		row.clear();
 
-	///*for the robot docs 1*/
-	//for (int i = 0; i < number_of_gr; ++i) {
-	//	for (int j = T; j < robot_chunks_count[i]; ++j) {
-	//		for (int k = 0; k < number_of_gr; ++k)
-	//			if (i != k)
-	//				for (int n = T; n < robot_chunks_count[k]; ++n)
-	//					row.push_back(
-	//						dzv_calc(
-	//							T, freq_of_robot_ngramm[i][j],
-	//							freq_of_robot_ngramm[k][n], 
-	//							j, n,
-	//							freq_of_robot_ngramm[i],
-	//							freq_of_robot_ngramm[k]
-	//						));
-	//				
-	//		/*for all the other docs*/
-	//		for (int k = 0; k < number_of_gr; ++k)
-	//			for (int n = T; n < human_chunks_count[k]; ++n)
-	//				row.push_back(
-	//					dzv_calc(
-	//						T, freq_of_robot_ngramm[i][j],
-	//						freq_of_human_ngramm[k][n],
-	//						j, n,
-	//						freq_of_robot_ngramm[i],
-	//						freq_of_human_ngramm[k]
-	//					));	
-	//		dzv_matrix.push_back(row);
-	//		row.clear();
-	//	}
-	//}
-	///*for the human docs*/
-	//for (int i = 0; i < number_of_gr; ++i) {
-	//	for (int j = T; j < human_chunks_count[i]; ++j) {
-	//		for (int k = 0; k < number_of_gr; ++k)
-	//			if (i != k)
-	//				for (int n = T; n < human_chunks_count[k]; ++n)
-	//					row.push_back(
-	//						dzv_calc(
-	//							T, freq_of_human_ngramm[i][j],
-	//							freq_of_human_ngramm[k][n],
-	//							j, n,
-	//							freq_of_human_ngramm[i],
-	//							freq_of_human_ngramm[k]
-	//						));
-
-	//		/*for all the other docs*/
-	//		for (int k = 0; k < number_of_gr; ++k)
-	//			for (int n = T; n < robot_chunks_count[k]; ++n)
-	//				row.push_back(
-	//					dzv_calc(
-	//						T, freq_of_human_ngramm[i][j],
-	//						freq_of_robot_ngramm[k][n],
-	//						j, n,
-	//						freq_of_human_ngramm[i],
-	//						freq_of_robot_ngramm[k]
-	//					));
-	//		dzv_matrix.push_back(row);
-	//		row.clear();
-	//	}
-	//}
+	}
 
 	///*write matrix with const columns 1*/
 
-	//std::ofstream write_matrix("matrix.txt");
-	//int colums = dzv_matrix[0].size();
-	//std::for_each(dzv_matrix.begin(), dzv_matrix.end(),
-	//	[&colums](vector<double>& col) mutable {
-	//		if (col.size() > colums) colums = col.size();
-	//	});
-	//for (vector<double> v : dzv_matrix) {
-	//	for (int i = 0; i < v.size(); ++i) {
-	//		write_matrix << v[i];
-	//		if (i != (v.size() - 1))
-	//			write_matrix << ' ';
-	//	}
-	//	for (int j = 0; j < colums - v.size(); ++j) {
-	//		write_matrix << ' ' << 0;
-	//		if (j != (colums - v.size() - 1))
-	//			write_matrix << ' ';
-	//	}
-	//	write_matrix << '\n';
-	//}
-	//write_matrix.close();
+	std::ofstream write_matrix("matrix.txt");
+	int colums = dzv_matrix[0].size();
+	std::for_each(dzv_matrix.begin(), dzv_matrix.end(),
+		[&colums](vector<double>& col) mutable {
+			if (col.size() > colums) colums = col.size();
+		});
+	for (vector<double> v : dzv_matrix) {
+		for (int i = 0; i < v.size(); ++i) {
+			write_matrix << v[i];
+			if (i != (v.size() - 1))
+				write_matrix << ' ';
+		}
+		for (int j = 0; j < colums - v.size(); ++j) {
+			write_matrix << ' ' << 0;
+			if (j != (colums - v.size() - 1))
+				write_matrix << ' ';
+		}
+		write_matrix << '\n';
+	}
+	write_matrix.close();
 
 	/*write matrix 1*/
 
